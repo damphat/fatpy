1152d7e77c5ca3f467506a9a1ebae461d70e653d
Phat <damphat@gmail.com>
2023-08-22 00:49:16 +0700

    plot3d

--- fat/plot.py
+++ fat/plot.py
@@ -4,6 +4,22 @@ import numpy as np
 from .lexer import Lexer
 from .parser import Parser
 
+def plot3d(f, *args, a = None, b = None, n=None, cmap='viridis', **kwargs):
+    plot.a = a if a is not None else plot.a
+    plot.b = b if b is not None else plot.b
+    plot.n = n if n is not None else plot.n
+    x = np.linspace(plot.a, plot.b, plot.n)
+    y = np.linspace(plot.a, plot.b, plot.n)
+    X, Y = np.meshgrid(x, y)
+    Z = f(X, Y)
+    ax = plt.subplot(111, projection='3d')
+    return ax.plot_surface(X, Y, Z, *args, cmap=cmap, **kwargs ) # type: ignore
+
+def show3d(f, *args, **kwargs):
+    if f is not None:
+        plot3d(f, *args, **kwargs)
+    return plt.show()
+
 def plot(*args, a = None, b = None, n=None, **kwargs):
     plot.a = a if a is not None else plot.a
     plot.b = b if b is not None else plot.b
